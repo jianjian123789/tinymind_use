2342e082dd10e210ed3a9071fec3acf60e9cbd85
wenjian <1902058165@qq.com>
2018-09-16 23:19:03 +0800

    Add files via upload

---
+++ tinymind使用.py
@@ -0,0 +1,11 @@
+from tinyenv.flags import flags
+
+# 所有在介面中輸入的參數都會被自動加載。
+FLAGS = flags()
+# 使用參數的方法和原來相同。
+print(FLAGS.iterations)  # 1000
+print(FLAGS.name)
+for i in range(100):
+    if i%5==0:
+        print(i)
+print(FLAGS.dir)
